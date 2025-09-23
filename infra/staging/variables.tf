42a5c9c58ed01249d7c0e267eed1a35126dd10dd
Shishir Dhakal <s224365862@deakin.edu.au>
2025-09-23 17:20:46 +1000

    Update variables.tf

--- infra/staging/variables.tf
+++ infra/staging/variables.tf
@@ -1,5 +1,22 @@
-variable "subscription_id" { type = string }
-variable "location"        { type = string  default = "australiaeast" }
-variable "project"         { type = string  default = "sit722" }
-variable "env"             { type = string  default = "staging" }
-variable "shared_acr_name" { type = string }  # existing ACR (permanent)
+variable "subscription_id" { 
+    type = string 
+    }
+
+variable "location" {
+     type = string  
+     default = "australiaeast" 
+     }
+
+variable "project" { 
+    type = string  
+    default = "sit722" 
+    }
+
+variable "env" { 
+    type = string  
+    default = "staging" 
+    }
+
+variable "shared_acr_name" {
+    type = string 
+    }  # existing ACR (permanent)
