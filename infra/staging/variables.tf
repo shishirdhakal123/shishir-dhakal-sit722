7845f430e88708d8af1efc3452ecd7fc84a95504
Shishir Dhakal <s224365862@deakin.edu.au>
2025-09-23 21:14:25 +1000

    Update CD Staging Workflow

--- infra/staging/variables.tf
+++ infra/staging/variables.tf
@@ -5,7 +5,7 @@ variable "subscription_id" {
 variable "prefix" {
   description = "Prefix for all resource names"
   type        = string
-  default     = "sit722-staging-dha"
+  default     = "sit722-staging-sdha"
 }
 
 variable "location" {
